477aade9e6dabe10da4a5aa6a53928c4bf28aaac
chavalk <chavalk@hotmail.com>
2021-04-27 17:26:30 -0500

    Add team names and logos to card.

--- src/components/ScoresCard/index.js
+++ src/components/ScoresCard/index.js
@@ -3,8 +3,17 @@ import Card from 'react-bootstrap/Card';
 
 const ScoresCard = () => {
     return (
-        <Card>
-            <Card.Body>This is some text within a card body.</Card.Body>
+        <Card border="secondary">
+            <Card.Body>
+                <Card.Title>
+                    <img className="mr-1" src="https://a1.espncdn.com/combiner/i?img=/i/teamlogos/nba/500/scoreboard/orl.png&h=70&w=70" alt="Orlando Magic Logo" style={{ maxWidth:"35px"}}/>
+                    Magic
+                </Card.Title>
+                <Card.Title>
+                    <img className="mr-1" src="https://a1.espncdn.com/combiner/i?img=/i/teamlogos/nba/500/scoreboard/cle.png&h=70&w=70" alt="Orlando Magic Logo" style={{ maxWidth:"35px"}}/>
+                    Cavaliers
+                </Card.Title>
+            </Card.Body>
         </Card>
     );
 }
