fa4351d9a3cb161b3502d540775847d02b16d747
chavalk <chavalk@hotmail.com>
2021-04-26 21:20:06 -0500

    Update navbar to only use necessary items from react-bootstrap library.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Nav from './components/Nav';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <Nav />
+      <NavBar />
     </div>
   );
 }
