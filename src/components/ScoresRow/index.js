8a223fa51438a6064ffe07e67d416d0d97a69810
chavalk <chavalk@hotmail.com>
2021-04-27 00:25:14 -0500

    Add style.css file to align h3 and add size to columns for better realignment on mobile.

--- src/components/ScoresRow/index.js
+++ src/components/ScoresRow/index.js
@@ -3,16 +3,17 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import ScoresCard from '../ScoresCard';
+import './style.css';
 
 const ScoresRow = (props) => {
     return (
-        <Container>
+        <Container className="mt-3">
             <h3>{props.league}</h3>
             <Row>
-                <Col><ScoresCard></ScoresCard></Col>
-                <Col><ScoresCard></ScoresCard></Col>
-                <Col><ScoresCard></ScoresCard></Col>
-                <Col><ScoresCard></ScoresCard></Col>
+                <Col md={3}><ScoresCard></ScoresCard></Col>
+                <Col md={3}><ScoresCard></ScoresCard></Col>
+                <Col md={3}><ScoresCard></ScoresCard></Col>
+                <Col md={3}><ScoresCard></ScoresCard></Col>
             </Row>
         </Container>
     );
