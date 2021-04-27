a792d9caf4894246ff70478965e4c298dcba810a
chavalk <chavalk@hotmail.com>
2021-04-26 23:23:23 -0500

    Add code for card and import it to ScoresRow.

--- src/components/ScoresCard/index.js
+++ src/components/ScoresCard/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
 
 const ScoresCard = () => {
     return (
-        <div></div>
+        <Card>
+            <Card.Body>This is some text within a card body.</Card.Body>
+        </Card>
     );
 }
 
--- src/components/ScoresRow/index.js
+++ src/components/ScoresRow/index.js
@@ -2,15 +2,16 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import ScoresCard from '../ScoresCard';
 
 const ScoresRow = () => {
     return (
         <Container>
             <Row>
-                <Col><h1>1</h1></Col>
-                <Col><h1>2</h1></Col>
-                <Col><h1>3</h1></Col>
-                <Col><h1>4</h1></Col>
+                <Col><ScoresCard></ScoresCard></Col>
+                <Col><ScoresCard></ScoresCard></Col>
+                <Col><ScoresCard></ScoresCard></Col>
+                <Col><ScoresCard></ScoresCard></Col>
             </Row>
         </Container>
     );
