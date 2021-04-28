ce7387a19822c03d2e96d38f5b58d67b4a3e6173
chavalk <chavalk@hotmail.com>
2021-04-27 22:37:27 -0500

    Add footer.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import NavBar from './components/NavBar';
 import Scores from './pages/Scores';
+import Footer from './components/Footer';
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-      <Scores />
+    <div className="page-container">
+      <div className="content-wrap">
+        <NavBar />
+        <Scores />
+      </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style.css';
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="container">
+                <hr />
+                <p>Copyright &copy; 2021 Scors Inc. All rights reserved. Privacy Policy | Terms of Use | Sales and Refunds | Legal | Site Map</p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
